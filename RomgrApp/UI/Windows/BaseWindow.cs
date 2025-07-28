2a8b7702ac9f5b15987d4d0d2fc3387d63841d86
thilong <thilong@126.com>
2025-07-28 17:16:40 +0800

    change window style

--- RomgrApp/UI/Windows/BaseWindow.cs
+++ RomgrApp/UI/Windows/BaseWindow.cs
@@ -5,5 +5,12 @@ namespace RomgrApp.UI.Windows;
 
 public class BaseWindow : Window
 {
-   
+    public void MinimizeCommand() => this.WindowState = WindowState.Minimized;
+    public void MaximizeCommand() => ToggleMaximize();
+    public void CloseCommand() => this.Close();
+    private void ToggleMaximize()
+    {
+        var window = this;
+        window.WindowState = window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+    }
 }
--- RomgrApp/UI/Windows/MainWindow.axaml.cs
+++ RomgrApp/UI/Windows/MainWindow.axaml.cs
@@ -1,11 +1,22 @@
 using Avalonia.Controls;
+using Avalonia.Platform;
 
 namespace RomgrApp.UI.Windows;
 
 public partial class MainWindow : BaseWindow
 {
+    
+    
     public MainWindow()
     {
         InitializeComponent();
+
+        WindowMinimizeActionButton.Click += (sender, args) => MinimizeCommand();
+        WindowMaximizeActionButton.Click += (sender, args) => MaximizeCommand();
+        WindowCloseActionButton.Click += (sender, args) => CloseCommand();
+        
+        
+        
     }
+    
 }
